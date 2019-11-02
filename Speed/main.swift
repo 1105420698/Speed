ec6787d5990a88cfc97926d415c78b00b56c5ce1
Runkai Zhang <1105420698@qq.com>
2019-11-01 23:05:32 -0400

    Initial Commit

---
+++ Speed/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Speed
+//
+//  Created by Runkai Zhang on 11/1/19.
+//  Copyright © 2019 Runkai Zhang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
